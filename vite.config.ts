ad3fbdf6a115728c2b5be65386634de931313abe
rob <roby.pakalns@gmail.com>
2026-01-06 22:11:16 +0200

    more optimization flags

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
     terserOptions: {
       compress: {
         passes: 7,
+        inline: 3,
+        booleans_as_integers: true,
+        reduce_funcs: true,
+        reduce_vars: true,
+      },
+      format: {
+        comments: false,
       },
       mangle: true,
     },
