504c8f90611115a396a247a179dffd94dfcc3231
robertpakalns <robertpakalns@users.noreply.github.com>
2026-01-13 18:13:01 +0000

    deploy: 22497cf01a9dfce05a0d940d17a1d6ce00d47fad

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import init from "../crates/app/pkg/app";
-import { mount } from "svelte";
-import App from "./App.svelte";
-import "./style.css";
-
-await init();
-
-mount(App, { target: document.getElementById("app")! });
