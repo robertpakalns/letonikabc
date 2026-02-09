b80d2323755f5bcfbbe66045fc9829527f61e445
rob <roby.pakalns@gmail.com>
2026-02-09 20:22:52 +0200

    update tests for the html parser

--- crates/parser/src/tests.rs
+++ crates/parser/src/tests.rs
@@ -3,6 +3,8 @@ mod tests {
     use crate::parse_html::parse;
     use alloc::vec;
 
+    // PARAGRAPHS
+
     #[test]
     fn simple_paragraph() {
         let html = "<p>Hello World!</p>";
@@ -22,30 +24,23 @@ mod tests {
     }
 
     #[test]
+    fn paragraph_with_extra_whitespace() {
+        let html = "<p>   Text with  no     big  spaces   </p>";
+        let (md, headers, _) = parse(html);
+
+        assert_eq!(md, "Text with no big spaces");
+        assert!(headers.is_empty());
+    }
+
+    // HEADERS
+
+    #[test]
     fn headers() {
         let html = "<h1>Title</h1><h2>Subtitle</h2><p>Text</p>";
         let (md, headers, _) = parse(html);
 
         assert_eq!(md, "# Title\n## Subtitle\nText");
         assert_eq!(headers, vec![1, 2]);
-    }
-
-    #[test]
-    fn span_and_br() {
-        let html = "<p>Line 1<br>Line 2<span>inside span</span></p>";
-        let (md, headers, _) = parse(html);
-
-        assert_eq!(md, "Line 1\nLine 2[inside span]");
-        assert!(headers.is_empty());
-    }
-
-    #[test]
-    fn empty_html() {
-        let html = "";
-        let (md, headers, _) = parse(html);
-
-        assert_eq!(md, "");
-        assert!(headers.is_empty());
     }
 
     #[test]
@@ -58,13 +53,26 @@ mod tests {
     }
 
     #[test]
-    fn multiple_line_breaks() {
-        let html = "<p>Line 1<br><br>Line 2</p>";
+    fn consecutive_headers() {
+        let html = "<h1>H1</h1><h2>H2</h2><h1>Another H1</h1>";
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md, "Line 1\n\nLine 2");
+        assert_eq!(md, "# H1\n## H2\n# Another H1");
+        assert_eq!(headers, vec![1, 2, 3]);
+    }
+
+    // STYLING (_italics_)
+
+    #[test]
+    fn paragraph_with_italics() {
+        let html = "<p>This is <i>italic</i> text and <i>more italics</i>.</p>";
+        let (md, headers, _) = parse(html);
+
+        assert_eq!(md, "This is _italic_ text and _more italics_.");
         assert!(headers.is_empty());
     }
+
+    // STYLING ([spans])
 
     #[test]
     fn paragraph_with_span_only() {
@@ -76,30 +84,64 @@ mod tests {
     }
 
     #[test]
-    fn consecutive_headers() {
-        let html = "<h1>H1</h1><h2>H2</h2><h1>Another H1</h1>";
+    fn nested_spans() {
+        let html = "<p>Start <span>middle <span>inner</span></span> end</p>";
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md, "# H1\n## H2\n# Another H1");
-        assert_eq!(headers, vec![1, 2, 3]);
+        assert_eq!(md, "Start [middle [inner]] end");
+        assert!(headers.is_empty());
     }
 
+    // STYLING (@[person spans])
+
     #[test]
-    fn paragraph_with_extra_whitespace() {
-        let html = "<p>   Text with  no     big  spaces   </p>";
+    fn person_span_simple() {
+        let html = r#"<p><span class="person">TRAI</span>: Hello.</p>"#;
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md, "Text with no big spaces");
+        assert_eq!(md, "@[TRAI]: Hello.");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn html_with_unsupported_tags() {
-        let html = "<p>Hello <em>World</em>!</p>";
+    fn multiple_person_spans() {
+        let html = r#"
+            <p><span class="person">PERSON1</span>: First line.</p>
+            <p><span class="person">PERSON2</span>: Second line.</p>
+        "#;
+
         let (md, headers, _) = parse(html);
 
-        // Ignore the tag
-        assert_eq!(md, "Hello World!");
+        assert_eq!(md, "@[PERSON1]: First line.\n@[PERSON2]: Second line.");
+        assert!(headers.is_empty());
+    }
+
+    #[test]
+    fn mixed_person_and_regular_span() {
+        let html = r#"<p><span class="person">PERSON1</span> meets <span>someone</span>.</p>"#;
+
+        let (md, headers, _) = parse(html);
+        assert_eq!(md, "@[PERSON1] meets [someone].");
+        assert!(headers.is_empty());
+    }
+
+    #[test]
+    fn nested_person_span() {
+        let html = r#"<p><span class="person">PERSON1 <span>Inner</span></span></p>"#;
+
+        let (md, headers, _) = parse(html);
+        assert_eq!(md, "@[PERSON1 [Inner]]");
+        assert!(headers.is_empty());
+    }
+
+    // MISCELLANEOUS
+
+    #[test]
+    fn empty_html() {
+        let html = "";
+        let (md, headers, _) = parse(html);
+
+        assert_eq!(md, "");
         assert!(headers.is_empty());
     }
 
@@ -113,63 +155,71 @@ mod tests {
     }
 
     #[test]
-    fn nested_spans() {
-        let html = "<p>Start <span>middle <span>inner</span></span> end</p>";
+    fn html_with_unsupported_tags() {
+        let html = "<p>Hello <em>World</em>!</p>";
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md, "Start [middle [inner]] end");
+        // Ignore the <em> tag because the function does not extract data from it
+        assert_eq!(md, "Hello World!");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn paragraph_with_italics() {
-        let html = "<p>This is <i>italic</i> text and <i>more italics</i>.</p>";
+    fn span_and_br() {
+        let html = "<p>Line 1<br>Line 2<span>inside span</span></p>";
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md, "This is _italic_ text and _more italics_.");
+        assert_eq!(md, "Line 1\nLine 2[inside span]");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn person_span_simple() {
-        let html = r#"<p><span class="person">TRAI</span>: Hello.</p>"#;
+    fn multiple_line_breaks() {
+        let html = "<p>Line 1<br><br>Line 2</p>";
         let (md, headers, _) = parse(html);
 
-        assert_eq!(md.trim(), "@[TRAI]: Hello.");
+        assert_eq!(md, "Line 1\n\nLine 2");
+        assert!(headers.is_empty());
+    }
+
+    // SECURITY
+    // JavaScript must not be present inside the markdown
+    // Because HTMl is inserted directly into DOM
+    // Otherwise readers are at risk of getting hacked
+
+    #[test]
+    fn js_injection_via_script() {
+        let html = r#"<script>alert(1)</script>"#;
+
+        let (md, headers, _) = parse(html);
+        assert_eq!(md, "");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn multiple_person_spans() {
-        let html = r#"
-            <p><span class="person">PERSON1</span>: First line.</p>
-            <p><span class="person">PERSON2</span>: Second line.</p>
-        "#;
+    fn js_injection_via_img_onerror() {
+        let html = r#"<img src=x onerror=alert(1) />"#;
 
         let (md, headers, _) = parse(html);
-
-        assert_eq!(
-            md.trim(),
-            "@[PERSON1]: First line.\n@[PERSON2]: Second line."
-        );
+        assert_eq!(md, "");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn mixed_person_and_regular_span() {
-        let html = r#"<p><span class="person">PERSON1</span> meets <span>someone</span>.</p>"#;
+    fn js_injection_via_a_src() {
+        let html = r#"<a href="javascript:alert(1)">Click me</a>"#;
 
         let (md, headers, _) = parse(html);
-        assert_eq!(md.trim(), "@[PERSON1] meets [someone].");
+        assert_eq!(md, "");
         assert!(headers.is_empty());
     }
 
     #[test]
-    fn nested_person_span() {
-        let html = r#"<p><span class="person">PERSON1 <span>Inner</span></span></p>"#;
+    fn js_injection_via_svg_onload() {
+        let html = r#"<svg onload=alert(1)></svg>"#;
 
         let (md, headers, _) = parse(html);
-        assert_eq!(md.trim(), "@[PERSON1 [Inner]]");
+        assert_eq!(md, "");
         assert!(headers.is_empty());
     }
 }
