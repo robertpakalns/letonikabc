d076a00451d2a6ef8e640aeee251841c98a31826
rob <roby.pakalns@gmail.com>
2026-03-21 22:07:47 +0200

    update all deps

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,7 @@ export default defineConfig({
   build: {
     target: "esnext",
     sourcemap: false,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        passes: 7,
-        inline: 3,
-        booleans_as_integers: true,
-        reduce_funcs: true,
-        reduce_vars: true,
-      },
-      format: {
-        comments: false,
-      },
-      mangle: true,
-    },
+    minify: true,
     modulePreload: {
       polyfill: false,
     },
